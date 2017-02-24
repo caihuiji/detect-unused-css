e3b4ca921eefa86282d36f51e15cc896091d9286
Chris Cai <caihuijigood@gmail.com>
2017-02-24 15:02:44 +0800

    rename

--- example/scandalLocal.js
+++
@@ -1,8 +0,0 @@
-var scandalLocal = require("../local/scandalLocal")
-var path = require("path");
-
-
-var include = ["*.html"]
-scandalLocal( path.join(__dirname , "test.css")  , path.join(__dirname ) , include);
-
-
